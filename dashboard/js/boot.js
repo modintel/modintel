3284f180cb71bb75b3278d1a51f2b7682ffeb78d
Natanim-Kemal <kemalnatanim@gmail.com>
2026-04-18 17:30:50 +0300

    refactor: minify boot.js

--- dashboard/js/boot.js
+++ dashboard/js/boot.js
@@ -1,13 +1,11 @@
 (function () {
     'use strict';
-
     if (typeof requireAuth === 'function') {
         const ok = requireAuth();
         if (!ok) {
             return;
         }
     }
-
     if (typeof attachLogoutButtons === 'function' && document.body?.dataset?.attachLogout === 'true') {
         attachLogoutButtons();
     }
