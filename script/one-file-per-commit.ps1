b1bf22963fc4d0b88839903edfb215d739280465
Natanim-Kemal <kemalnatanim@gmail.com>
2026-04-18 13:00:26 +0300

    refactor: remove old one-file-per-commit script

--- script/one-file-per-commit.ps1
+++
@@ -1,6 +0,0 @@
-$files = git diff --cached --name-only --diff-filter=ACM
-if ($files.Count -gt 1) {
-    Write-Host "ERROR: Multiple files in commit. Each commit should have only 1 file."
-    Write-Host "Files staged: $($files -join ', ')"
-    exit 1
-}
