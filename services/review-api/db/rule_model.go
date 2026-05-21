aa05e664d9cc6545fa244327c1f4ae5c8b8b0112
Natanim-Kemal <kemalnatanim@gmail.com>
2026-05-21 19:54:26 +0300

    feat: add signature field to waful and auto-regenerate custom rules conf

--- services/review-api/db/rule_model.go
+++ services/review-api/db/rule_model.go
@@ -3,16 +3,17 @@ package db
 import "time"
 
 type WAFRule struct {
-	ID             string    `bson:"id" json:"id"`
-	Type           string    `bson:"type" json:"type"`
-	Category       string    `bson:"category" json:"category"`
-	Description    string    `bson:"description" json:"description"`
-	Severity       string    `bson:"severity" json:"severity"`
-	Phase          int       `bson:"phase" json:"phase"`
-	ParanoiaLevel  int       `bson:"paranoia_level" json:"paranoia_level"`
-	Source         string    `bson:"source" json:"source"`
-	Enabled        bool      `bson:"enabled" json:"enabled"`
-	Archived       bool      `bson:"archived" json:"archived"`
-	CreatedAt      time.Time `bson:"created_at" json:"created_at"`
-	UpdatedAt      time.Time `bson:"updated_at" json:"updated_at"`
+	ID            string    `bson:"id" json:"id"`
+	Type          string    `bson:"type" json:"type"`
+	Category      string    `bson:"category" json:"category"`
+	Description   string    `bson:"description" json:"description"`
+	Severity      string    `bson:"severity" json:"severity"`
+	Phase         int       `bson:"phase" json:"phase"`
+	ParanoiaLevel int       `bson:"paranoia_level" json:"paranoia_level"`
+	Source        string    `bson:"source" json:"source"`
+	Enabled       bool      `bson:"enabled" json:"enabled"`
+	Archived      bool      `bson:"archived" json:"archived"`
+	CreatedAt     time.Time `bson:"created_at" json:"created_at"`
+	UpdatedAt     time.Time `bson:"updated_at" json:"updated_at"`
+	Signature     string    `bson:"signature" json:"signature"`
 }
